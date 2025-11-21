59ec780ba9384d51a73f020877358a7bc8f78994
Haouo <q18131500@gs.ncku.edu.tw>
2025-11-22 02:13:21 +0800

    Add flake.nix for devShell with Nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Basic compilation shell environment for writing in Typst!";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              typst
+            ];
+          };
+        }
+      );
+    };
+}
